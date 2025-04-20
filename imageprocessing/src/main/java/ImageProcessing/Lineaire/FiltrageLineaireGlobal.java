d726775fc5c5ae74794426c9b94add3903d4ade7
iRyann <ryanbouchou@orange.fr>
2025-04-20 15:55:07 +0200

    Update : compilation configuration and empty Linear filtering addition.

---
+++ imageprocessing/src/main/java/ImageProcessing/Lineaire/FiltrageLineaireGlobal.java
@@ -0,0 +1,21 @@
+package ImageProcessing.Lineaire;
+
+public class FiltrageLineaireGlobal {
+
+    public static int[][] filtrePasseBasIdeal(int[][] image, int frequenceCoupure){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static int[][] filtrePasseHautIdeal(int[][] image, int frequenceCoupure) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static int[][] filtrePasseBasButterworth(int[][] image, int frequenceCoupure, int ordre){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static int[][] filtrePasseHautButterworth(int[][] image, int frequenceCoupure, int ordre){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
