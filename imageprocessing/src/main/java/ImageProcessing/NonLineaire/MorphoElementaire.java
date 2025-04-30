3b9ec42cd933e6638fe3b6a09f6aabcc9a198f71
iRyann <ryanbouchou@orange.fr>
2025-04-30 17:08:35 +0200

    TODO

---
+++ imageprocessing/src/main/java/ImageProcessing/NonLineaire/MorphoElementaire.java
@@ -0,0 +1,29 @@
+package ImageProcessing.NonLineaire;
+
+import ImageProcessing.Complexe.MatriceComplexe;
+
+import javax.management.RuntimeErrorException;
+
+import ImageProcessing.Complexe.Complexe;
+import ImageProcessing.Fourier.Fourier;
+import ImageProcessing.Utilities.MyCast;
+
+public class MorphoElementaire {
+
+    public static int[][] erosion(int [][] image,int tailleMasque){
+        // TODO - implement MorphoElementaire.erosion
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    public static int[][] dilatation(int [][] image,int tailleMasque){
+        // TODO - implement MorphoElementaire.dilatation
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    public static int[][] ouverture(int [][] image,int tailleMasque){
+        // TODO - implement MorphoElementaire.ouverture
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    public static int[][] fermeture(int [][] image,int tailleMasque){
+        // TODO - implement MorphoElementaire.fermeture
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
