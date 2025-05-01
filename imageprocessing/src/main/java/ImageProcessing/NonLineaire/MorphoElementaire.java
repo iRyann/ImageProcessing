a8029996399d0f61df10065f08d88d2f4fa1d81c
iRyann <ryanbouchou@orange.fr>
2025-05-01 12:12:35 +0200

    Update : Érosion

--- imageprocessing/src/main/java/ImageProcessing/NonLineaire/MorphoElementaire.java
+++ imageprocessing/src/main/java/ImageProcessing/NonLineaire/MorphoElementaire.java
@@ -11,9 +11,31 @@ import ImageProcessing.Utilities.MyCast;
 public class MorphoElementaire {
 
     public static int[][] erosion(int [][] image,int tailleMasque){
-        // TODO - implement MorphoElementaire.erosion
-        throw new UnsupportedOperationException("Not yet implemented");
+        int half = tailleMasque / 2;
+        int[][] res = new int[image.length][image[0].length];
+        
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[0].length; j++) {
+                if (i < half || i >= image.length - half || j < half || j >= image[0].length - half) {
+                    res[i][j] = 0;
+                    continue;
+                }
+
+                int min = 255;
+                for (int k = -half; k <= half; k++) {
+                    for (int l = -half; l <= half; l++) {
+                        if (image[i + k][j + l] < min) {
+                            min = image[i + k][j + l];
+                        }
+                    }
+                }
+                res[i][j] = min;
+            }
+        }
+        
+        return res;
     }
+
     public static int[][] dilatation(int [][] image,int tailleMasque){
         // TODO - implement MorphoElementaire.dilatation
         throw new UnsupportedOperationException("Not yet implemented");
