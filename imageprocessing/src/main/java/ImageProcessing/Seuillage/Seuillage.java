9a2d7befe733a660a255a5a06f81115125f11a14
iRyann <ryanbouchou@orange.fr>
2025-06-16 17:44:57 +0200

    Implement input validation and enhance automatic thresholding in seuillageAutomatiqueSafe method

--- imageprocessing/src/main/java/ImageProcessing/Seuillage/Seuillage.java
+++ imageprocessing/src/main/java/ImageProcessing/Seuillage/Seuillage.java
@@ -9,9 +9,9 @@ public class Seuillage {
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < columns; j++){
                 if(image[i][j] >= seuil){
-                    imageSeuillee[i][j] = 255;
-                } else {
                     imageSeuillee[i][j] = 0;
+                } else {
+                    imageSeuillee[i][j] = 255;
                 }
             }
         }
@@ -94,5 +94,63 @@ public class Seuillage {
             }
         }
         return imageSeuillee;
+    }
+
+    public static int[][] seuillageAutomatiqueSafe(int[][] image) {
+        if (image == null || image.length == 0 || image[0].length == 0) {
+            throw new IllegalArgumentException("Image vide ou nulle");
+        }
+        
+        // Calcul du seuil initial (moyenne)
+        long sum = 0;
+        int totalPixels = image.length * image[0].length;
+        
+        for(int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[i].length; j++) {
+                sum += image[i][j];
+            }
+        }
+        int seuil = (int)(sum / totalPixels);
+
+        boolean changed = true;
+        int iterations = 0;
+        final int MAX_ITERATIONS = 100; // Protection contre boucles infinies
+        
+        do {
+            if (++iterations > MAX_ITERATIONS) {
+                System.err.println("Seuillage automatique: convergence non atteinte après " + MAX_ITERATIONS + " itérations");
+                break;
+            }
+            
+            long sum1 = 0, sum2 = 0;
+            int count1 = 0, count2 = 0;
+
+            for(int i = 0; i < image.length; i++) {
+                for (int j = 0; j < image[i].length; j++) {
+                    if(image[i][j] <= seuil) {
+                        sum1 += image[i][j];
+                        count1++;
+                    } else {
+                        sum2 += image[i][j];
+                        count2++;
+                    }
+                }
+            }
+
+            // Protection contre division par zéro
+            if (count1 == 0 || count2 == 0) {
+                break; // Cas dégénéré, garder le seuil actuel
+            }
+            
+            int newSeuil = (int)((sum1 / count1 + sum2 / count2) / 2);
+
+            if(newSeuil == seuil) {
+                changed = false;
+            } else {
+                seuil = newSeuil;
+            }
+        } while(changed);
+
+        return seuillageSimple(image, seuil);
     }
 }
